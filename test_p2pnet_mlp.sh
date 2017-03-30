555515abeb45a7d3e542c2d851a286f1df20edd6
fegin <fegins@gmail.com>
2017-03-30 15:55:46 -0400

    Test script udpate.

--- test_p2pnet_mlp.sh
+++ test_p2pnet_mlp.sh
@@ -6,8 +6,8 @@ ignored=5
 export KMP_AFFINITY="explicit,granularity=fine,proclist=[0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30]"
 export NNVM_EXEC_MATCH_RANGE=0
 export MXNET_P2PNET_DEBUG=1
-python tests/python/unittest/test_p2pnet_mlp.py -f all_private_ips -b $batch -w $weight -l $layers -n $iterations -g $ignored -i $1 -e l${layers}_comm_$1 | tee l${layers}_comm_$1
-#export MXNET_P2PNET_DEBUG=3
-#python tests/python/unittest/test_p2pnet_mlp.py -f all_private_ips -b $batch -w $weight -l $layers -n $iterations -g $ignored -i $1 -e l${layers}_no_comm_$1 | tee l${layers}_no_comm_$1
-#export MXNET_P2PNET_DEBUG=1
-#python tests/python/unittest/test_p2pnet_mlp.py -s -b $batch -w $weight -l $layers -n $iterations -g $ignored -e l${layers}_single_$1 | tee l${layers}_single_$1
+python tests/python/unittest/test_p2pnet_mlp.py -f all_ips -b $batch -w $weight -l $layers -n $iterations -g $ignored -i $1 -e l${layers}_comm_$1 | tee l${layers}_comm_$1
+export MXNET_P2PNET_DEBUG=3
+python tests/python/unittest/test_p2pnet_mlp.py -f all_ips -b $batch -w $weight -l $layers -n $iterations -g $ignored -i $1 -e l${layers}_no_comm_$1 | tee l${layers}_no_comm_$1
+export MXNET_P2PNET_DEBUG=1
+python tests/python/unittest/test_p2pnet_mlp.py -s -b $batch -w $weight -l $layers -n $iterations -g $ignored -e l${layers}_single_$1 | tee l${layers}_single_$1
