c39bc5df061d2407e6254a9f6f6238571914c9ba
fegin <fegins@gmail.com>
2017-04-08 12:20:48 +0000

    Change thr scripts to fit Azure A8.

---
+++ mpi_env.sh
@@ -0,0 +1,7 @@
+export I_MPI_FABRICS=shm:dapl
+export I_MPI_DAPL_PROVIDER=ofa-v2-ib0
+export I_MPI_DYNAMIC_CONNECTION=0
+export LD_LIBRARY_PATH=/opt/intel/lib/intel64:$LD_LIBRARY_PATH
+export PYTHONPATH=/home/tofu/mxnet/python
+export NNVM_EXEC_MATCH_RANGE=0
+export OMP_NUM_THREADS=8
