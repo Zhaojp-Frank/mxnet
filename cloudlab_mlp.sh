51e7efde8a878b8b67fe17d85f6f978cd621ccbb
fegin <fegin@node-0.tofu-perf.tofu-pg0.apt.emulab.net>
2017-09-03 10:03:36 -0600

    cloudlab test script

--- cloudlab_mlp.sh
+++ cloudlab_mlp.sh
@@ -20,23 +20,24 @@ OPTIONS=$OPTIONS" -x MXNET_P2PNET_MPI_POLLING_TIME=1 "
 OPTIONS=$OPTIONS" -x MXNET_P2PNET_MAIN_THREAD_AFFINITY=14 "
 #OPTIONS=$OPTIONS" -x MXNET_P2PNET_MPI_TEST_METHOD=1 "
 OPTIONS=$OPTIONS" -x MXNET_P2PNET_COMMUNICATION_METHOD=MPI "
-OPTIONS=$OPTIONS" -x NNVM_EXEC_MATCH_RANGE=1 "
+OPTIONS=$OPTIONS" -x NNVM_EXEC_MATCH_RANGE=0 "
 OPTIONS=$OPTIONS" -x MXNET_P2PNET_INTERNAL_POLLING=1 "
 OPTIONS=$OPTIONS" -x TOFU_NO_COMPUTATION=0 "
+OPTIONS=$OPTIONS" -x TOFU_FAKE_VAR_SPLIT_CONCAT=1 "
 CMD="python tofu_test_mlp.py --batch_size=${B} --hidden_size=${H} --num_layers=5 "
 
-#echo "Doing $NP $B $H single"
+echo "Doing $NP $B $H single"
 #mpirun $OPTIONS -output-filename log_mlp_single -x MXNET_P2PNET_DEBUG=0 /local/mxnet/env.sh
-#mpirun $OPTIONS -output-filename log_mlp_single -x MXNET_P2PNET_DEBUG=0 $CMD --address=127.0.0.1 -i 0
+mpirun $OPTIONS -output-filename log_mlp_single -x MXNET_P2PNET_DEBUG=0 $CMD --address=127.0.0.1 -i 0
 
-#echo "Doing $NP $B $H without communication"
-#mpirun $OPTIONS -output-filename log_mlp_without_comm_${NP}_${B}_${H} -x MXNET_P2PNET_DEBUG=2 -x TOFU_TILING_TYPE=kcuts $CMD -f $HOST 
+echo "Doing $NP $B $H without communication"
+mpirun $OPTIONS -output-filename log_mlp_without_comm_${NP}_${B}_${H} -x MXNET_P2PNET_DEBUG=2 -x TOFU_TILING_TYPE=kcuts $CMD -f $HOST 
 
 echo "Doing $NP $B $H with communication"
-mpirun $OPTIONS -output-filename log_mlp_with_comm_${NP}_${B}_${H} -x MXNET_P2PNET_DEBUG=0 -x TOFU_TILING_TYPE=kcuts $CMD -f $HOST
+mpirun $OPTIONS -output-filename log_mlp_with_comm_${NP}_${B}_${H} -x MXNET_P2PNET_DEBUG=9 -x TOFU_TILING_TYPE=kcuts $CMD -f $HOST
 
-#echo "Doing $NP $B $H dp without communication"
-#mpirun $OPTIONS -output-filename log_mlp_dp_without_comm_${NP}_${B}_${H} -x MXNET_P2PNET_DEBUG=2 -x TOFU_TILING_TYPE=datapar $CMD -f $HOST
+echo "Doing $NP $B $H dp without communication"
+mpirun $OPTIONS -output-filename log_mlp_dp_without_comm_${NP}_${B}_${H} -x MXNET_P2PNET_DEBUG=2 -x TOFU_TILING_TYPE=datapar $CMD -f $HOST
 
-#echo "Doing $NP $B $H dp with communication"
-#mpirun $OPTIONS -output-filename log_mlp_with_comm_${NP}_${B}_${H} -x MXNET_P2PNET_DEBUG=0 -x TOFU_TILING_TYPE=datapar $CMD -f $HOST
+echo "Doing $NP $B $H dp with communication"
+mpirun $OPTIONS -output-filename log_mlp_dp_with_comm_${NP}_${B}_${H} -x MXNET_P2PNET_DEBUG=9 -x TOFU_TILING_TYPE=datapar $CMD -f $HOST
