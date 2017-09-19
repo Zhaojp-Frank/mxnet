19b04dad93e9920e8990575c099ba62e22198dfe
Minjie Wang <wmjlyjemaine@gmail.com>
2017-09-19 04:06:00 +0000

    update mlp test script

--- tofu_gpu_tests/scripts/mlp.sh
+++ tofu_gpu_tests/scripts/mlp.sh
@@ -1,4 +1,4 @@
-n=2
+n=4
 
 #echo "Single: "
 #for b in 512 1024 2048 4096 8192
@@ -12,44 +12,64 @@ n=2
 #  done
 #done
 
-echo "Tofu: #GPUs=$n"
+#echo "Tofu: #GPUs=$n"
+#export TOFU_FAKE_VAR_SPLIT_CONCAT=1
+#export TOFU_TILING_TYPE=kcuts
+#for b in 512 1024 2048 4096 8192
+#do
+#  for h in 512 1024 2048 4096 8192
+#  do
+#    echo "Batch: $b, Hidden: $h"
+#    python ../tofu_benchmark.py mlp \
+#      --hidden_size=$h --batch_size=$b --num_gpus=$n \
+#      2>&1 | tee mlp_b${b}_h${h}_tofu_${n}.log | grep average
+#  done
+#done
+#
+#echo "DP: #GPUs=$n"
+#export TOFU_FAKE_VAR_SPLIT_CONCAT=1
+#export TOFU_TILING_TYPE=datapar
+#for b in 512 1024 2048 4096 8192
+#do
+#  for h in 512 1024 2048 4096 8192
+#  do
+#    echo "Batch: $b, Hidden: $h"
+#    python ../tofu_benchmark.py mlp \
+#      --hidden_size=$h --batch_size=$b --num_gpus=$n \
+#      2>&1 | tee mlp_b${b}_h${h}_dp_${n}.log | grep average
+#  done
+#done
+
+echo "Tofu-no-comm: #GPUs=$n"
 export TOFU_FAKE_VAR_SPLIT_CONCAT=1
 export TOFU_TILING_TYPE=kcuts
+export TOFU_IGNORE_GPU_COMM=1
 for b in 512 1024 2048 4096 8192
 do
   for h in 512 1024 2048 4096 8192
   do
+    echo "Batch: $b, Hidden: $h"
     python ../tofu_benchmark.py mlp \
       --hidden_size=$h --batch_size=$b --num_gpus=$n \
-      2>&1 | tee mlp_b${b}_h${h}_tofu_${n}.log | grep average
+      2>&1 | tee mlp-no-comm_b${b}_h${h}_tofu_${n}.log | grep average
   done
 done
 
-echo "DP: #GPUs=$n"
+echo "DP-no-comm: #GPUs=$n"
 export TOFU_FAKE_VAR_SPLIT_CONCAT=1
 export TOFU_TILING_TYPE=datapar
+export TOFU_IGNORE_GPU_COMM=1
 for b in 512 1024 2048 4096 8192
 do
   for h in 512 1024 2048 4096 8192
   do
+    echo "Batch: $b, Hidden: $h"
     python ../tofu_benchmark.py mlp \
       --hidden_size=$h --batch_size=$b --num_gpus=$n \
-      2>&1 | tee mlp_b${b}_h${h}_dp_${n}.log | grep average
+      2>&1 | tee mlp-no-comm_b${b}_h${h}_dp_${n}.log | grep average
   done
 done
 
-#echo "Tofu-no-comm: "
-#TOFU_FAKE_VAR_SPLIT_CONCAT=1 TOFU_TILING_TYPE=kcuts TOFU_IGNORE_GPU_COMM=1 \
-#  python ../tofu_benchmark.py miniresnet \
-#  --num_gpus=2 --batch_size=$b --num_filter=$f --image_size=$i \
-#  2>&1 | grep average
-#
-#echo "DP-no-comm: "
-#TOFU_FAKE_VAR_SPLIT_CONCAT=1 TOFU_TILING_TYPE=datapar TOFU_IGNORE_GPU_COMM=1 \
-#  python ../tofu_benchmark.py miniresnet \
-#  --num_gpus=2 --batch_size=$b --num_filter=$f --image_size=$i \
-#  2>&1 | grep average
-#
 #echo "Half-batch-single: "
 #python ../tofu_benchmark.py miniresnet \
 #  --num_gpus=1 --batch_size=$(($b/2)) --num_filter=$f --image_size=$i \
