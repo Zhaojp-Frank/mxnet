18c02f559ea6fa13c4cfa6b1bc8b50f5fd4bb42b
Minjie Wang <wmjlyjemaine@gmail.com>
2018-04-08 23:40:19 +0000

    k-equal-cut and spartan strategy test script

--- tofu_gpu_tests/scripts/wresnet.sh
+++ tofu_gpu_tests/scripts/wresnet.sh
@@ -42,8 +42,52 @@ export TOFU_USE_BFS_LEVEL=1
 #  done
 #done
 
-export TOFU_IGNORE_GPU_COMM=1
-echo "Tofu-2GPUs-no-comm:"
+#export TOFU_IGNORE_GPU_COMM=1
+#echo "Tofu-2GPUs-no-comm:"
+#export TOFU_FAKE_VAR_SPLIT_CONCAT=1
+#export TOFU_TILING_TYPE=kcuts
+#for b in 2 4 8 16 32 64
+#do
+#  for w in 1 2 4 8
+#  do
+#    echo "Batch: $b, Wide: $w"
+#    python ../tofu_benchmark.py resnet \
+#      --batch_size=$b --num_layers=$l --wide_scale=$w --num_gpus=2 \
+#      2>&1 | tee resnet${l}-no-comm_b${b}_w${w}_tofu_2.log | grep average
+#  done
+#done
+#
+#echo "Tofu-4GPUs-no-comm:"
+#export TOFU_FAKE_VAR_SPLIT_CONCAT=1
+#export TOFU_TILING_TYPE=kcuts
+#for b in 4 8 16 32 64 128
+#do
+#  for w in 1 2 4 8
+#  do
+#    echo "Batch: $b, Wide: $w"
+#    python ../tofu_benchmark.py resnet \
+#      --batch_size=$b --num_layers=$l --wide_scale=$w --num_gpus=4 \
+#      2>&1 | tee resnet${l}-no-comm_b${b}_w${w}_tofu_4.log | grep average
+#  done
+#done
+
+#export TOFU_TILINE_TYPE="k-equal-cuts"
+#echo "Tofu-4GPUs-k-equal-cut:"
+#export TOFU_FAKE_VAR_SPLIT_CONCAT=1
+#export TOFU_TILING_TYPE=kcuts
+#for b in 4 8 16 32 64 128
+#do
+#  for w in 1 2 4 8
+#  do
+#    echo "Batch: $b, Wide: $w"
+#    python ../tofu_benchmark.py resnet \
+#      --batch_size=$b --num_layers=$l --wide_scale=$w --num_gpus=4 \
+#      2>&1 | tee resnet${l}_b${b}_w${w}_equal-cut_4.log | grep average
+#  done
+#done
+
+export TOFU_TILINE_TYPE="spartan"
+echo "Tofu-2GPUs-spartan:"
 export TOFU_FAKE_VAR_SPLIT_CONCAT=1
 export TOFU_TILING_TYPE=kcuts
 for b in 2 4 8 16 32 64
@@ -53,11 +97,11 @@ do
     echo "Batch: $b, Wide: $w"
     python ../tofu_benchmark.py resnet \
       --batch_size=$b --num_layers=$l --wide_scale=$w --num_gpus=2 \
-      2>&1 | tee resnet${l}-no-comm_b${b}_w${w}_tofu_2.log | grep average
+      2>&1 | tee resnet${l}_b${b}_w${w}_spartan_2.log | grep average
   done
 done
 
-echo "Tofu-4GPUs-no-comm:"
+echo "Tofu-4GPUs-spartan:"
 export TOFU_FAKE_VAR_SPLIT_CONCAT=1
 export TOFU_TILING_TYPE=kcuts
 for b in 4 8 16 32 64 128
@@ -67,7 +111,7 @@ do
     echo "Batch: $b, Wide: $w"
     python ../tofu_benchmark.py resnet \
       --batch_size=$b --num_layers=$l --wide_scale=$w --num_gpus=4 \
-      2>&1 | tee resnet${l}-no-comm_b${b}_w${w}_tofu_4.log | grep average
+      2>&1 | tee resnet${l}_b${b}_w${w}_spartan_4.log | grep average
   done
 done
 
