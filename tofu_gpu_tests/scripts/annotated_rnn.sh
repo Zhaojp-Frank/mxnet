ed29489c3b3a750cf69f8460310090466d7b3dc1
Minjie Wang <wmjlyjemaine@gmail.com>
2018-04-24 05:23:45 +0000

    fix CPU memory issue when using large LSTM

--- tofu_gpu_tests/scripts/annotated_rnn.sh
+++ tofu_gpu_tests/scripts/annotated_rnn.sh
@@ -1,9 +1,11 @@
-l=2
+l=8
 s=20
 
 export TOFU_ENABLED=1
 export TOFU_USE_BFS_LEVEL=1
 export TOFU_FAKE_VAR_SPLIT_CONCAT=1
+#export MXNET_EXEC_INPLACE_GRAD_SUM_CAP=1
+#export TOFU_FUSED_CONVERSION=1
 
 #echo "Single: "
 #for b in 1 2 4 8 16 32 64 128 256
@@ -30,13 +32,13 @@ export TOFU_FAKE_VAR_SPLIT_CONCAT=1
 #done
 
 echo "4GPUs: "
-for b in 64
+for b in 4 8 16 32 64 128 256
 do
   for h in 8192
   do
     echo "Batch: $b, Hidden: $h"
     python ../tofu_annotated_lstm.py --batch_size=$b --num_layers=$l \
       --hidden_size=$h --seq_len=$s --num_gpus=4 \
-      2>&1 | tee lstm${l}-${s}_b${b}_h${h}_tofu_4.log | grep average
+      2>&1 | tee lstm${l}-${s}_b${b}_h${h}_tofu_3.log | grep average
   done
 done
