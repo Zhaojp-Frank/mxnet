a6bf75d1d76820547cd58b223b085e432e628f54
Minjie Wang <wmjlyjemaine@gmail.com>
2018-04-24 02:06:12 +0000

    update script

--- tofu_gpu_tests/scripts/annotated_rnn.sh
+++ tofu_gpu_tests/scripts/annotated_rnn.sh
@@ -17,26 +17,26 @@ export TOFU_FAKE_VAR_SPLIT_CONCAT=1
 #  done
 #done
 
-echo "2GPUs: "
-for b in 32
+#echo "2GPUs: "
+#for b in 32
+#do
+#  for h in 8192
+#  do
+#    echo "Batch: $b, Hidden: $h"
+#    python ../tofu_annotated_lstm.py --batch_size=$b --num_layers=$l \
+#      --hidden_size=$h --seq_len=$s --num_gpus=2 \
+#      2>&1 | tee lstm${l}-${s}_b${b}_h${h}_tofu_2.log | grep average
+#  done
+#done
+
+echo "4GPUs: "
+for b in 64
 do
   for h in 8192
   do
     echo "Batch: $b, Hidden: $h"
     python ../tofu_annotated_lstm.py --batch_size=$b --num_layers=$l \
-      --hidden_size=$h --seq_len=$s --num_gpus=2
-      #2>&1 | tee lstm${l}-${s}_b${b}_h${h}_tofu_2.log | grep average
+      --hidden_size=$h --seq_len=$s --num_gpus=4 \
+      2>&1 | tee lstm${l}-${s}_b${b}_h${h}_tofu_4.log | grep average
   done
 done
-
-#echo "4GPUs: "
-#for b in 1 2 4 8 16 32 64 128 256 512
-#do
-#  for h in 8192
-#  do
-#    echo "Batch: $b, Hidden: $h"
-#    python ../tofu_lstm_benchmark.py --batch_size=$b --num_layers=$l \
-#      --hidden_size=$h --seq_len=$s --num_loops=15 --num_gpus=4 \
-#      2>&1 | tee lstm${l}-${s}_b${b}_h${h}_4.log | grep average
-#  done
-#done
