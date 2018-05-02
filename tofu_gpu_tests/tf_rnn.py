75fd9bcd0df4b402931cbaee16a2c1ccbf84b454
fegin <fegins@gmail.com>
2018-05-02 14:01:05 -0400

    tf_rnn

---
+++ tofu_gpu_tests/tf_rnn.py
@@ -0,0 +1,43 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.contrib import layers
+from tensorflow.contrib import rnn
+
+def RNN(x, n_layers=6, n_hidden=8192, n_seq=20, n_class=512):
+    x = tf.split(x, n_seq, 1)
+    for i in range(n_layers):
+        with tf.device('/device:GPU:{}'.format(i)):
+            with tf.variable_scope('lstm_layer_{}'.format(i)) as scope:
+                rnn_cell = rnn.BasicLSTMCell(n_hidden)
+                outputs, states = rnn.static_rnn(rnn_cell, x, dtype=tf.float32)
+                x = outputs
+    outputs = [layers.fully_connected(o, n_class, biases_initializer=None)
+                for o in outputs]
+    outputs = [layers.softmax(o) for o in outputs]
+    # return tf.group(*outputs)
+    return tf.add_n(outputs)
+
+
+def run(n_batch=32, n_layers=6, n_seq=20, n_embed=512, n_class=512):
+    with tf.device('/device:GPU:0'):
+        data = tf.placeholder(tf.float32, (n_batch, n_embed * n_seq))
+        loss = RNN(data, n_layers=n_layers, n_seq=n_seq, n_class=n_class)
+        optimizer = tf.train.GradientDescentOptimizer(0.01)
+        train_op = optimizer.minimize(loss)
+    x = np.random.rand(n_batch, n_embed * n_seq)
+    sess = tf.Session()
+    sess.run(tf.global_variables_initializer())
+    print('-------------------')
+    for i in range(10):
+        begin = time.time()
+        sess.run(train_op, feed_dict={data:x})
+        end = time.time()
+        print(end - begin)
+
+
+if __name__ == '__main__':
+    run(n_batch=32,
+        n_layers=1,
+        n_seq=20,
+        n_embed=512,
+        n_class=512)
