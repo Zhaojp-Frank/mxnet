e5ea4239bdce1a8ed0300759bb23c3e7ae4546d8
KarlLeell <cl3940@nyu.edu>
2018-07-25 19:57:49 +0000

    commit two shell scripts

--- benchmark_tests/lstm.sh
+++ benchmark_tests/lstm.sh
@@ -1,4 +1,4 @@
-# Suggested Setting: Layers=269 Batch_size=4 Wide_scale=4 
+# Suggested Setting: ./lstm.sh 32 30 2 1024
 BATCH_SIZE=$1
 SEQ_LEN=$2
 LAYERS=$3
@@ -17,8 +17,8 @@ echo "HIDDEN_SIZE = ${HIDDEN_SIZE}"
 
 export SWAP_ALGORITHM=NaiveHistory
 export MXNET_ENGINE_TYPE=NaiveEngine
-export PYTHONPATH=/home/karl/debug_naive/incubator-mxnet/python
+export PYTHONPATH=/home/karl/incubator-mxnet/python
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64
-export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=0
 
 python tofu_lstm_benchmark.py --cold_skip=${COLD_SKIP} --input_size=${INPUT_SIZE} --num_classes=${CLASSES} --use_momentum=${USE_MOMENTUM} --num_gpus=${NUM_GPU} --hidden_size=${HIDDEN_SIZE} --num_layers=${LAYERS} --batch_size=${BATCH_SIZE} --seq_len=${SEQ_LEN} --num_loop=${NUM_LOOPS} > log_lstm_${LAYERS}_${BATCH_SIZE}_${SEQ_LEN}_${HIDDEN_SIZE}
--- benchmark_tests/run.sh
+++ benchmark_tests/run.sh
@@ -7,10 +7,11 @@ echo "LAYERS = ${LAYERS}"
 echo "BATCH_SIZE = ${BATCH_SIZE}"
 echo "WIDE_SCALE = ${WIDE_SCALE}"
 
+export PREFETCH_STEP_AHEAD=50
 export SWAP_ALGORITHM=NaiveHistory
 export MXNET_ENGINE_TYPE=NaiveEngine
-export PYTHONPATH=/home/sotskin/incubator-mxnet/python
+export PYTHONPATH=/home/karl/incubator-mxnet/python
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64
-export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=0
 
 python benchmark.py --num_gpus=1 --num_layers=${LAYERS} --batch_size=${BATCH_SIZE} --wide_scale=${WIDE_SCALE} --num_loop=6 resnet > log_resnet_${LAYERS}_4_${WIDE_SCALE}
