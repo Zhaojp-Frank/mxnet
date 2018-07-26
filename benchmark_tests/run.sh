12dfd1dbcd280451be3b1d6025565966f9547698
Sotskin <sotskin@gmail.com>
2018-07-26 14:43:55 +0000

    update run.sh from main branch

--- benchmark_tests/run.sh
+++ benchmark_tests/run.sh
@@ -16,6 +16,6 @@ export SWAP_ALGORITHM=${SWAP}
 export MXNET_ENGINE_TYPE=ThreadedEngine
 export PYTHONPATH=/home/karl/incubator-mxnet/python
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64
-export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=0
 
 python benchmark.py --num_gpus=1 --num_layers=${LAYERS} --batch_size=${BATCH_SIZE} --wide_scale=${WIDE_SCALE} --num_loop=10 resnet > log_resnet_${LAYERS}_${BATCH_SIZE}_${WIDE_SCALE}_${SWAP}_${PREFETCH}_${STEPS}
