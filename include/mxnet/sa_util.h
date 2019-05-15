df073afb57301a2b3ea2fd73d92b2b3f626d875e
Chien-Chin Huang <fegins@gmail.com>
2019-05-15 18:17:54 +0000

    New swapadvisor log system.

---
+++ include/mxnet/sa_util.h
@@ -0,0 +1,28 @@
+#ifndef MXNET_SA_UTIL_H_
+#define MXNET_SA_UTIL_H_
+
+#include <sstream>
+#include <thread>
+
+#if 1
+#define sa_log \
+  SA_Log().GetStream()
+#else
+#define sa_log \
+  if (false) SA_Log().GetStream()
+#endif
+
+class SA_Log {
+ public:
+  explicit SA_Log() { 
+    id_ = std::this_thread::get_id();
+    sstream_ << "[" << id_ << "] ";
+  }
+  std::ostringstream& GetStream() { return sstream_; }
+  ~SA_Log() { std::cout << sstream_.str(); }
+
+ private:
+  std::ostringstream sstream_;
+  std::thread::id id_;
+};
+#endif
