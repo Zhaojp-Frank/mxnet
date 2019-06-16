fff16df10fd313b9c751fcc2190e4bed518369f7
Derek Jin <Sotskin@users.noreply.github.com>
2019-06-16 16:38:38 -0400

    Swap advisor on-demand (#24)
    The new design of on-demand swap. The prefetch is now a standalone thread and will stop when no handles can be swapped in. This design also gets rid of the look-ahead variable.

--- src/storage/gpu_odswap.h
+++ src/storage/gpu_odswap.h
@@ -6,12 +6,12 @@
 #include <memory>
 #include <pthread.h>
 #include <thread>
+#include <semaphore.h>
 #include <stack>
 #include <string>
 #include <unistd.h>
 #include <unordered_map>
-#include <queue>
-#include <set>
+#include <unordered_set>
 #if MXNET_USE_CUDA
 #include <cuda_runtime.h>
 #endif // MXNET_USE_CUDA
@@ -22,6 +22,7 @@ namespace mxnet {
 
 struct SwapInfo {
   handle_t handle_id;
+  bool is_weight;
   bool swapped_in;
   int device_id;
   void* dptr;
@@ -29,44 +30,7 @@ struct SwapInfo {
   size_t size;
   size_t swap_count;
   std::atomic_flag is_swapping;
-};
-
-
-using SIGroup = std::vector<SwapInfo*>;
-class SwapInfoGroups {
-public:
-  ~SwapInfoGroups();
-  static SwapInfoGroups* Get();
-  static std::shared_ptr<SwapInfoGroups> _GetSharedRef();
-  void NewInfo(void* addr, SwapInfo* info);
-  std::shared_ptr<SIGroup> SwapOut(void* addr);
-  std::shared_ptr<SIGroup> SwapIn(void* addr, SwapInfo* info);
-
-private:
-  SwapInfoGroups();
-  std::unordered_map<void*, std::shared_ptr<SIGroup>> in_memory_;
-  std::unordered_map<SwapInfo*, std::shared_ptr<SIGroup>> all_;
-};
-
-class ThreadAccessInfo {
-public:
-  static const int kAccessThreshold = 0;
-  static const unsigned kInvalidID = 2147483648;
-
-  ~ThreadAccessInfo();
-  static ThreadAccessInfo* Get();
-  static std::shared_ptr<ThreadAccessInfo> _GetSharedRef();
-  std::set<handle_t>& CheckAndCreate(handle_t hid, bool access, bool& running);
-  handle_t Access(handle_t hid);
-  void Remove(handle_t hid);
-
-private:
-  ThreadAccessInfo();
-  std::unordered_map<std::thread::id, std::set<handle_t>> all_threads_;
-  std::unordered_map<handle_t,
-                     std::unordered_set<std::thread::id>> hid_to_threads_;
-  std::unordered_map<std::thread::id, bool> is_running;
-  unsigned running_threshold_;
+  bool is_waiting;
 };
 
 class ODSwap {
@@ -74,18 +38,18 @@ public:
   ~ODSwap();
   static ODSwap* Get();
   static std::shared_ptr<ODSwap> _GetSharedRef();
-  void SwapOut(unsigned required_memory, int device_id, bool async);
+  bool SwapOut(unsigned required_memory, int device_id, bool async);
   void SwapOutLocked(unsigned required_memory, int device_id, bool async);
-  void SwapIn(SwapInfo *info, bool async);
-  void SetAddr(handle_t handle_id, void* dptr, size_t size, int device_id);
+  bool SwapIn(SwapInfo *info, bool async);
+  void SetAddr(handle_t handle_id, void* dptr, size_t size, 
+               int device_id, bool is_pre, bool is_weight);
   void DelAddr(handle_t handle_id);
   void FreeAddr(handle_t handle_id);
-  void* GetAddr(handle_t handle_id, bool prefetch = false);
-  unsigned AccessID() {
-    return access_id_.fetch_add(1, std::memory_order_relaxed);
-  }
-  void PrePostAccess(bool is_pre);
-
+  void* GetAddr(handle_t handle_id, bool is_prefetch, bool& success);
+  void LockHandles(const std::unordered_set<handle_t>& handles, const size_t node_idx);
+  void LockHandles(const std::vector<handle_t>& handles, const size_t node_idx);
+  void UnlockHandles(const std::unordered_set<handle_t>& handles, const size_t node_idx);
+  void CheckUnlocked();
 #if 0
   void LockSwap();
   void UnlockSwap();
@@ -96,20 +60,18 @@ private:
   ODSwap();
   // FIXME(fegin): The design of the following two variables doesn't support
   // multiple GPUs.
-  std::shared_ptr<SwapInfoGroups> swapinfo_groups_;
-  std::shared_ptr<ThreadAccessInfo> thread_info_;
   std::unordered_map<handle_t, SwapInfo*> swap_info_;
   std::unordered_set<handle_t> swappable_handles_[NUMBER_OF_GPU];
+  std::unordered_map<handle_t, std::unordered_set<size_t>> locked_handles_;
   std::map<size_t, std::unordered_set<handle_t> > divided_handles_[NUMBER_OF_GPU];
-  std::stack<handle_t> locked_handles_[NUMBER_OF_GPU];
-  std::vector<size_t> free_memory_;
   std::shared_ptr<MemoryHistory> memory_history_;
   std::shared_ptr<MemoryManager> memory_manager_;
-  std::atomic<unsigned> access_id_;
+  sem_t swap_sem_;
   pthread_rwlock_t swap_lock_;
-  pthread_rwlock_t locks_[NUMBER_OF_GPU];
   bool swap_async_;
   bool infinite_memory_;
+  bool infinite_cpu_memory_;
+  char* fake_cpu_address_;  
   cudaStream_t streams_out_[NUMBER_OF_GPU];
   cudaStream_t streams_in_[NUMBER_OF_GPU];
 }; // Class Swap
--- src/storage/gpu_swap_history.h
+++ src/storage/gpu_swap_history.h
@@ -9,6 +9,7 @@
 #include <vector>
 #include <thread>
 #include <mutex>
+#include <mxnet/sa_util.h>
 
 #if MXNET_USE_CUDA
 #include <cuda_runtime.h>
@@ -18,7 +19,7 @@ using namespace std::chrono;
 
 namespace mxnet {
 
-using handle_t = unsigned long long;
+using handle_t = unsigned long;
 using timestamp_t = unsigned long long;
 using timestep_t = unsigned long long;
 
@@ -58,7 +59,9 @@ public:
     size_t swap_out_total;
     size_t num_get_addr;
   };
-  static const size_t kBeginRecordAt = 2;
+  // Iteration 0: Binding stage, no record
+  // Iteration 1: Record History
+  static const size_t kBeginRecordAt = 1;
 
   ~MemoryHistory();
   static bool CompareByStep(const MemRecord &r1, const MemRecord &r2) {
@@ -67,16 +70,20 @@ public:
   static MemoryHistory* Get();
   static std::shared_ptr<MemoryHistory> _GetSharedRef();
   bool IterationStarted() {return iteration_started_;}
+  bool IsPreparing() {return iteration_idx_ == 0;}
   bool IsPreRecording() {return pre_recording_;}
   bool IsRecording() {return is_recording_;}
   DeviceHistory& DevHistory(int device) {return dev_history_[device];}
-  size_t GetIterationIdx() {return iteration_idx_;}
+  size_t& GetIterationIdx() {return iteration_idx_;}
   void PreRecord(handle_t handle_id, record_t op, DeviceHistory& history);
   void PutRecord(handle_t handle_id, int device, record_t op, size_t size);
   void PrintRecord(int device);
+  void StartPreparation();
+  void EndPreparation();
   void StartIteration();
   void StopIteration();
   void Statistics();
+  size_t GetCacheMiss();
   handle_t DecideVictim(std::unordered_set<handle_t> handles, int device,
                            void* arg);
 
